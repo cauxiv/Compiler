0be7b0f7cab71eaa906a6485ad37837ed8d94760
sangryul <ksl970330@naver.com>
2021-06-05 03:00:28 +0900

    first check done

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -2,10 +2,12 @@ import sys
 
 class Analyzer:
   def __init__(self):
-    self.trace_symbol_stack = [0]
+    self.trace_symbol_stack = ['0']
     self.trace_input = []
     self.trace_input_code = []
 
+    #slr_table
+    self.slr_table = []
     #slr_table에서 읽어야할 state(row)
     self.slr_table_state = []
     #slr_table에서 읽어야할 column
@@ -17,17 +19,17 @@ class Analyzer:
 
     self.cursor = 0
     self.action_state = ''
+    self.last_state_num = ''
     self.stack_top = ''
     self.input_top = ''
     #stack_top과 input_top를 비교해서 action_state를 구하고 그에 따른 행동을 해야한다.
 
   def read_slr_table(self,row, column):
     action=''
-    for x, i in enumerate(self.slr_table_state):
-      for y in self.slr_table_type:
-        if(x == row and y == column):
-          action = self.slr_table_do[int(i)]
-          break
+    for x in self.slr_table:
+      r,c,v = x.split(",")
+      if((r == row) and (c == column)):
+        action = v
     return action
         
 
@@ -36,7 +38,6 @@ class Analyzer:
     self.stack_top = self.trace_symbol_stack[-1]
     self.input_top = self.trace_input[0]
     self.action_state = self.read_slr_table(self.stack_top, self.input_top)
-    print(self.stack_top, self.input_top)
     print(self.action_state)
 
     if(self.action_state == ''):
@@ -46,33 +47,42 @@ class Analyzer:
 
     elif(self.action_state[0] == 'r'):
       #reduce
-      print("R")
-      pass
+      #reduce_rule에 적혀있는 index에 해당하는 길이 만큼 stack에서 pop을 한뒤 해당 symbol을 넣는다.
+      reduce_rule_index = self.action_state[1:]
+      pop_length = int(self.reduce_rule[reduce_rule_index][0]) * 2
+
+      self.trace_symbol_stack = self.trace_symbol_stack[:(len(self.trace_symbol_stack) - pop_length)]
+      top = self.reduce_rule[reduce_rule_index][1]
+
+      last_num = self.trace_symbol_stack[-1]
+      #문자 들어오기 이전 number
+      self.trace_symbol_stack.append(top)
+      
+      goto_num = self.read_slr_table(last_num,top)
+      self.trace_symbol_stack.append(goto_num)
+      #do parsing
+      self.parse()
+      
     
     elif(self.action_state[0] == 'a'):
       #accept
-      print("A")
+      print("Accept 되었습니다.")
       pass
     elif(self.action_state[0] == 's'):
-      #error
+      #state_to_to_num
       self.trace_symbol_stack.append(self.input_top)
-      self.trace_symbol_stack.append(self.action_state[1])
+      self.last_state_num = self.action_state[1:]
+      self.trace_symbol_stack.append(self.last_state_num)
       self.trace_input = self.trace_input[1:]
+      #do parsing
       self.parse()
-      pass
-    else:
-      #goto number
-      pass
     
 
   def prepare_slr_table(self):
     with open("slr_table.csv", 'r') as f:
       parse_slr_table = f.read().splitlines()
       for x in parse_slr_table:
-        table_split = x.split(",")
-        self.slr_table_state.append(table_split[0])
-        self.slr_table_type.append(table_split[1])
-        self.slr_table_do.append(table_split[2])
+        self.slr_table.append(x)
 
   def prepare_reduce_rule(self):
     with open("reduce_rule.csv", "r") as f:
