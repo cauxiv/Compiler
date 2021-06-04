2bfb16d3cea316ef6e3fc3df09ba6bfe88ff9edf
sangryul <ksl970330@naver.com>
2021-06-04 19:41:23 +0900

    check merge conflict

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -1,8 +1,33 @@
+import sys
+
+class Analyzer():
+    
+    symbol_stack = []
+    code_stack = []
+
+    def __init__(self):
+        pass
+
+
 def syntax_analyzer():
-    print("start")
+    
+    '''
+    if len(sys.argv) != 2:
+        print("Insufficient arguments")
+        sys.exit()
+
+    file_name = sys.argv[1]
+    
+    '''
+    analyzer = Analyzer()
+
+    with open('input_stream3.txt', 'r') as f:
+        lines = f.readlines()
+        print(lines)
 
 
 
 
 if __name__ == "__main__":
+    
     syntax_analyzer()
