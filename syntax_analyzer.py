f6a8ab3dc604c5bd397573c5d04aff97c789392d
sangryul <ksl970330@naver.com>
2021-06-05 00:42:01 +0900

    add some parsing information table

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -1,29 +1,44 @@
 import sys
 
-class Analyzer():
-    
-    symbol_stack = []
-    code_stack = []
+class Analyzer:
+  def __init__(self):
+    self.trace_symbol_stack = [0]
+    self.trace_input_stack = ['$']
 
-    def __init__(self):
-        pass
+    #slr_table에서 읽어야할 state(row)
+    self.slr_table_state = []
+    #slr_table에서 읽어야할 column
+    self.slr_table_type = []
+    #slr_table에서 실제 하는것들
+    self.slr_table_do = [] 
 
+def prepare_slr_table():
+  pass
+
+def prepare_reduce_rule():
+  pass
 
 def syntax_analyzer():
     
-    '''
-    if len(sys.argv) != 2:
-        print("Insufficient arguments")
-        sys.exit()
+  '''
+  if len(sys.argv) != 2:
+    print("Insufficient arguments")
+    sys.exit()
 
-    file_name = sys.argv[1]
+  file_name = sys.argv[1]
+  
+  '''
+  analyzer = Analyzer()
+
+  with open('symbol_table.txt', 'r') as f:
+    parse_list = f.read().splitlines()
+      
+    for x in parse_list:
+      symbol, code = x.split(",")
+      analyzer.symbol_stack.append(symbol)
+      analyzer.code_stack.append(code)
     
-    '''
-    analyzer = Analyzer()
 
-    with open('input_stream3.txt', 'r') as f:
-        lines = f.readlines()
-        print(lines)
 
 
 
